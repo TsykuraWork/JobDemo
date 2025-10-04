741aaffa7b70a58568ecf0e5ec1d0b5246a56e55
TsykuraWork <tsykurawork@gmail.com>
2025-10-04 10:50:52 +0300

    Добавил пример кода алгоритма MD5

---
+++ Labs | Inf Security/MD5_Alg/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vvedite stroky: ");
+            string input = Console.ReadLine();
+            MD5 objectName = new MD5(input);
+            Console.Write(objectName.GenHash());
+            Console.ReadLine();
+        }
+    }
+}
